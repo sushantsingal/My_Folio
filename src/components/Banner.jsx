248a52946f32065b383622a1c383a607a32191d9
Sushant Singal <sushantsingal42@gmail.com>
2026-04-25 22:21:44 +0530

    l

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,4 @@
-6import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.png";
